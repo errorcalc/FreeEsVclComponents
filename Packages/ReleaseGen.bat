b269582b766a016b4d16755bd407053ef802c053
erorsoft <errorsoft@mail.ru>
2026-03-20 05:23:12 +0400

    new pckages

--- Packages/ReleaseGen.bat
+++ Packages/ReleaseGen.bat
@@ -24,35 +24,10 @@ call :vclwinx_before_fix
 set package=RX10Rio
 call :vclwinx_before_fix
 
-rem fix vclwinx package requires (need after Sydney)
-set package=RX10Sydney
-call :vclwinx_after_fix
-set package=RX11Alexandria
-call :vclwinx_after_fix
-set package=RX12Athens
-call :vclwinx_after_fix
-
 goto :end
 
 :vclwinx_before_fix
-FileFix -path %package%/EsVclComponents.dpk -delete-with "{$IF not(Defined(VER210) or Defined(VER220) or Defined(VER230) or Defined(VER240) or Defined(VER250) or Defined(VER260) or Defined(VER270) or Defined(VER280) or Defined(VER290) or Defined(VER300) or Defined(VER310) or Defined(VER320) or Defined(VER330))}vclwinx,{$IFEND}"
-exit /b
-
-rem set %package% var!
-type %package%\EsVclComponents.dpk | findstr /v vclwinx > %package%\temp.dpk
-del %package%\EsVclComponents.dpk
-rename %package%\temp.dpk EsVclComponents.dpk
-exit /b
-
-:vclwinx_after_fix
-FileFix -path %package%/EsVclComponents.dpk -replace-with "{$IF not(Defined(VER210) or Defined(VER220) or Defined(VER230) or Defined(VER240) or Defined(VER250) or Defined(VER260) or Defined(VER270) or Defined(VER280) or Defined(VER290) or Defined(VER300) or Defined(VER310) or Defined(VER320) or Defined(VER330))}vclwinx,{$IFEND}" -to "vclwinx,"
-set lineto=/"RX10Sydney/"
-FileFix -path %package%/EsVclComponents.dproj -insert-after "<DCCReference Include=^^EsCore.dcp^^/>" -line "<DCCReference Include=^^vclwinx.dcp^^/>"
-exit /b
-
-rem set %package% var!
-powershell -executionpolicy bypass -command "(Get-Content -Path %package%\EsVclComponents.dpk) -replace [regex]::Escape('{$IF not(Defined(VER210) or Defined(VER220) or Defined(VER230) or Defined(VER240) or Defined(VER250) or Defined(VER260) or Defined(VER270) or Defined(VER280) or Defined(VER290) or Defined(VER300) or Defined(VER310) or Defined(VER320) or Defined(VER330))}vclwinx,{$IFEND}'), 'vclwinx,' | Set-Content -Path %package%\EsVclComponents.dpk -Encoding UTF8"
-powershell -executionpolicy bypass -command "(Get-Content -Path %package%\EsVclComponents.dproj) -replace [regex]::Escape('        <DCCReference Include=\"EsCore.dcp\"/>'), ('        <DCCReference Include=\"EsCore.dcp\"/>' + \"`r`n\" + '        <DCCReference Include=\"vclwinx.dcp\"/>') | Set-Content -Path %package%\EsVclComponents.dproj -Encoding UTF8"
+FileFix -path %package%/EsVclComponents.dpk -delete-with "vclwinx,"
 exit /b
 
 :end
